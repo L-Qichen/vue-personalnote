1034e568c073092a42f8e466c1772751ff3bdf3f
Toby <5722599@gmail.com>
2022-09-10 10:39:19 -0400

    css initialized, sidebar component WIP.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <sidebar />
+    <router-view />
   </div>
 </template>
 
 <script>
+import sidebar from "@/components/SideBar.vue";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    sidebar,
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+*::before {
+  box-sizing: border-box;
+}
+
+*::after {
+  box-sizing: border-box;
+}
+
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100%;
+}
+
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
+}
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul,
+ol {
+  list-style: none;
+}
+
+button {
+  font-size: 14px;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  color: #444;
+}
+
+#app {
+  display: flex;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="sidebar">
+    <div class="icons">
+      <router-link to="/note/1">
+        <div class="iconfont icon-note">icon</div>
+      </router-link>
+      <router-link to="/notebooks">
+        <div class="iconfont icon-notebook">icon</div>
+      </router-link>
+      <router-link to="/trash/2">
+        <div class="iconfont icon-trash">icon</div>
+      </router-link>
+      <div class="logout">
+        <button class="iconfont icon-logout" @click="onLogout">icon</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style scoped>
+#sidebar {
+  border: 1px solid red;
+}
+#sidebar > div .iconfont {
+  border: 1px solid orange;
+}
+.logout {
+  border: 1px solid blue;
+}
+</style>
